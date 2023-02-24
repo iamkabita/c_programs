618343071642f3429e8e3b5d50f79d31421fc6db
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-02-24 22:00:16 +0530

    practice some programs

--- projects/array_pointer.c
+++ projects/array_pointer.c
@@ -1,8 +1,47 @@
 #include<stdio.h>
 #include<conio.h>
 
+void input(int *p)
+{
+	int i;
+	for(i=0;i<=4;i++)
+	{
+		scanf("%d",p+i);
+	}
+}
+
+void display(int *p)
+{
+	int i;
+	for(i=0;i<=4;i++)
+	{
+		printf("%d ",*(p+1));
+	}
+}
+
+void sort(int *p)
+{
+	int round,t,i;
+	for(round=1;round<=4;round++)
+	{
+		for(i=0;i<=3;i++)
+		{
+			if(*(p+i) > *(p+i+1))
+			{
+				t = *(p+i);
+			    *(p+i) = *(p+i+1);
+			    *(p+i+1) = t;
+			}
+		}
+	}
+}
 void main()
 {
-
-	
+	int a[5];
+	input(a);
+	display(a);
+	sort(a);
+	display(a);
+	getch();
 }
+
