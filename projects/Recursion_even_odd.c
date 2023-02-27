5c82c4fdf139f8f77756efa6c3032861c5bd9f5a
Kabita Barnwal <barnwalkabita1@gmail.com>
2023-02-27 16:23:40 +0530

    practice some problem

--- projects/Recursion_even_odd.c
+++ projects/Recursion_even_odd.c
@@ -10,12 +10,22 @@ int sumEven(int n)
 		return (s);
 	}
 }
+int sumOdd(int n)
+{
+	int s;
+	if (n>0)
+	{
+		s = n*2 +1 + sumOdd(n-1);
+		return (s);
+	}
+}
 void main()
 {
 	int x,m;
 	printf("Enter a number\n");
 	scanf("%d",&x);
 	m = sumEven(x);
-	printf("%d",m);
+	printf("Sum of %d Even numbers is %d",x,m);
+	printf("\n\nSum of %d Odd numbers is %d",x,sumOdd(x));
 	getch();
 }
